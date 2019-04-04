d18bde0b82322027304f570cb0c1c19a5e843203
Westermann <fwestermann@me.com>
2019-04-04 20:28:30 +0200

    Add some tests

---
+++ PlayGroundFritz/TestUnits/TestDllfunc.pas
@@ -0,0 +1,32 @@
+﻿namespace ProHolz.CodeGen;
+
+const TestDll = "
+Unit TestDll;
+interface
+const
+   {$IF Defined(WIN32)}
+// LIB_CAIRO_32 = 'libCairo-2.dll';
+   {$IFDEF DEBUGCAIRO}
+   LIB_CAIRO_32 = 'CairoD.dll';
+   {$ELSE}
+   LIB_CAIRO_32 = 'Cairo.dll';
+   {$ENDIF}
+   _PU = '';
+   {$ELSE}
+   {$MESSAGE Error 'Unsupported platform'}
+   {$ENDIF}
+ function cairo_version(): Integer; cdecl; external LIB_CAIRO_32 name 'cairo_version';
+
+function cairo_interfaceimplementation(): Integer; cdecl;
+
+implementation
+
+// Will only show up in the interface in Oxygene
+function cairo_interfaceimplementation(): Integer; cdecl; external LIB_CAIRO_32 name 'cairo_interfaceimplementation';
+
+function cairo_implementation(): Integer; cdecl; external LIB_CAIRO_32 name 'cairo_implementation';
+end.
+
+";
+
+end.
---
+++ PlayGroundFritz/TestUnits/TestWithClause.pas
@@ -0,0 +1,24 @@
+﻿namespace ProHolz.CodeGen;
+const TestWithClause = "
+unit TestMethods;
+
+interface
+
+implementation
+ procedure test;
+ begin
+
+   with value do
+     begin
+       test := test+1;
+
+     end;
+
+ end;
+
+
+end.
+
+
+";
+end.
